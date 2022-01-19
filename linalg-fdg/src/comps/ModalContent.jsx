ede51ac7796ef326d3bbbb6ae31a57d27203040e
Huxley Marvit <huxmarv@nuevaschool.org>
2022-01-19 04:15:27 +0000

    codemirrrooor

--- linalg-fdg/src/comps/ModalContent.jsx
+++ linalg-fdg/src/comps/ModalContent.jsx
@@ -1,12 +1,28 @@
 import { useState, useRef, useCallback } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
+import { languages } from '@codemirror/language-data';
+import { oneDark } from '@codemirror/theme-one-dark';
 
 const ModalContent = (props) => {
     //console.log(Object.keys(props.node), props.node, "the content??")
     //console.log(props.node.content)
+    const [ev, setEv] = useState(props.node.content)
 
     return (
-	<div>
-	    {props.node.content}
+	<div className="h-full border-2 border-red-400">
+	    {/*{props.node.content}*/}
+	    <CodeMirror
+		value={ev} 
+		theme={oneDark}
+		//className="h-full"
+		onChange={(e) => {
+		    setEv(e)
+		}}
+		extensions={[
+		    markdown({ base: markdownLanguage, codeLanguages: languages })
+		]} 
+	    />
 	</div>
     );
 }
