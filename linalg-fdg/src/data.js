0df0c8e08ba955ce93843daf99da646a400575fe
Huxley Marvit <huxmarv@nuevaschool.org>
2022-01-19 22:37:47 -0800

    new data (deserved commit fs)

--- linalg-fdg/src/data.js
+++ linalg-fdg/src/data.js
@@ -1,3 +1,518 @@
+//let data = {
+//    "nodes": [
+//        {
+//            "id": "Vectors",
+//            "group": 1,
+//            "content":  "# Vectors \n A vector is a list of values."
+//        },
+
+//        {
+//            "id": "Linear Independence",
+//            "group": 1,
+//            "content":  "# Linear Independence \n A list of vectors is linearly independent if the only way to create a linear combination equal to zero is to set all constants multiplying the vectors to zero."
+//        },
+//        {
+//            "id": "Linear Dependence",
+//            "group": 1,
+//            "content":  "# Linear Dependence \n A list of vectors is linearly dependent if there is a linear combination with not all coefficients equal to 0 that can result in a combination equal to 0."
+//        },
+
+//        {
+//            "id": "Linear Combination",
+//            "group": 1,
+//            "content":  "# Linear Combination \n A linear combination consists of adding scalar multiples of the vectors in your list."
+//        },
+
+//        {
+//            "id": "Span",
+//            "group": 1,
+//            "content":  "# Span \n The span of a list of vectors is the collection of all linear combinations of those vectors."
+//        },
+
+//        {
+//            "id": "Bases",
+//            "group": 1,
+//            "content":  "# Bases \n A basis of V is a list of vectors in V that is linearly independent and spans V."
+//        },
+
+//        {
+//"id": "Vector Space",
+//"group": 1,
+//"content":  "# Vector Space \n A vector space is a collection of vectors that has the following properties: commutativity, associativity, additive identity, additive inverse multiplicative identity, and distributive properties"
+//        },
+
+//        {
+//"id": "Commutativity",
+//"group": 1,
+//"content":  "# Commutativity \n An operation is commutative when a + b = b + a."
+//        },
+
+//        {
+//"id": "Associativity",
+//"group": 1,
+//"content":  "# Associativity \n An operation is associative when (a + b) + c = a + (b + c)."
+//        },
+
+//        {
+//"id": "Distributivity",
+//"group": 1,
+//"content":  "# Distributivity \n An operation is distributive when a * (b + c) = a * b + a * c."
+//        },
+
+//        {
+//"id": "Additive Identity",
+//"group": 1,
+//"content":  "# Additive Identity \n An additive identity is a value x such that y + x = y. "
+//        },
+
+//        {
+//"id": "Additive Inverse",
+//"group": 1,
+//"content":  "# Additive Inverse \n A vector space has an additive inverse if for every x, there is a y where x + y = 0."
+//        },
+
+//        {
+//"id": "Multiplicative Identity",
+//"group": 1,
+//"content":  "# Multiplicative Identity \n A multiplicative identity is a value x such that xy = y. "
+//        },
+
+//        {
+//"id": "Subspaces",
+//"group": 1,
+//"content":  "# Subspaces \n A subspace is a subset of a vector space that itself is also a vector space. "
+//        },
+
+//        {
+//"id": "Direct Sums",
+//"group": 1,
+//"content":  "# Direct Sums \n EMPTY"
+//        },
+
+//        {
+//"id": "Linear Maps",
+//"group": 1,
+//"content":  "# Linear Maps \n EMPTY"
+//        },
+
+//        {
+//"id": "Matrix Representation of Linear Maps",
+//"group": 1,
+//"content":  "# Matrix Representation of Linear Maps \n EMPTY"
+//        },
+
+//        {
+//"id": "Invertibility",
+//"group": 1,
+//"content":  "# Invertibility \n EMPTY"
+//        },
+
+//        {
+//"id": "Injectivity",
+//"group": 1,
+//"content":  "# Injectivity \n EMPTY"
+//        },
+
+//        {
+//"id": "Surjectivity",
+//"group": 1,
+//"content":  "# Surjectivity \n EMPTY"
+//        },
+
+//        {
+//"id": "Null Space",
+//"group": 1,
+//"content":  "# Null Space \n EMPTY"
+//        },
+
+//        {
+//"id": "Range",
+//"group": 1,
+//"content":  "# Range \n EMPTY"
+//        },
+
+//        {
+//"id": "Communication",
+//"group": 2,
+//"content":  "# Communication \n EMPTY"
+//        },
+
+//        {
+//"id": "If/Only if Proofs",
+//"group": 2,
+//"content":  "# If/Only if Proofs \n EMPTY"
+//        },
+
+//        {
+//"id": "Direct Proofs",
+//"group": 2,
+//"content":  "# Direct Proofs \n EMPTY"
+//        },
+
+//        {
+//"id": "Diagrams",
+//"group": 2,
+//"content":  "# Diagrams \n EMPTY"
+//        },
+
+//        {
+//"id": "Proof by Contradiction",
+//"group": 2,
+//"content":  "# Proof by Contradiction \n EMPTY"
+//        },
+
+//        {
+//            "id": "Explanation",
+//            "group": 2,
+//            "content":  "# Explanation \n EMPTY"
+//        },
+
+//        {
+//"id": "Eigenvalues",
+//"group": 1,
+//"content":  "# Eigenvalues \n EMPTY"
+//        },
+
+//        {
+//"id": "Eigenvectors",
+//"group": 1,
+//"content":  "# Eigenvectors \n EMPTY"
+//        },
+
+//        {
+//"id": "Invariant Subspaces",
+//"group": 1,
+//"content":  "# Invariant Subspaces \n EMPTY"
+//        },
+
+//        {
+//"id": "Determinant",
+//"group": 1,
+//"content":  "# Determinant \n "
+//        },
+
+//        {
+//"id": "Proof by Contrapositive",
+//"group": 1,
+//"content":  "# Proof by Contrapositive \n "
+//        },
+
+//        {
+//"id": "Groups",
+//"group": 1,
+//"content":  "# Groups \n "
+//        },
+
+//    ],
+//    "links": [
+
+
+//        {
+//"source": "Linear Independence",
+//"target": "Linear Dependence",
+//"value": 1,
+//"Content":  "Linear Independence and Linear Dependence are opposite terms dealing with the same concept of unique information. A Linearly Independent list contains vectors that each provide some form of new information, whereas in a Linearly Dependent list there are vectors that provide only repetitive information: they are in the span of the other vectors."
+//        },
+
+//        {
+//"source": "Linear Independence",
+//"target": "Linear Combination",
+//"value": 1,
+//"Content":  "The concept of Linear Independence relies on Linear Combinations to define the interactions between vectors. Linear Independence is defined on a Linear Combination of vectors in a list, set to equal zero."
+//        },
+
+//        {
+//"source": "Linear Dependence",
+//"target": "Linear Combination",
+//"value": 1,
+//"Content":  "The concept of Linear Dependence relies on Linear Combinations to define the interactions between vectors. Linear Dependence is defined on a Linear Combination of vectors in a list, set to equal zero."
+//        },
+
+//        {
+//"source": "Span",
+//"target": "Linear Combination",
+//"value": 1,
+//"Content":  "The definition of Span relies on Linear Combinations of vectors in a list."
+//        },
+
+//        {
+//"source": "Bases",
+//"target": "Linear Independence",
+//"value": 1,
+//"Content":  "A key property of a basis is Linear Independence."
+//        },
+
+//        {
+//"source": "Bases",
+//"target": "Span",
+//"value": 1,
+//"Content":  "One key property of a basis is that it spans the space it is in."
+//        },
+
+//        {
+//"source": "Vector Space",
+//"target": "Commutativity",
+//"value": 1,
+//"Content":  "Vector Spaces are Commutative."
+//        },
+
+//        {
+//"source": "Vector Space",
+//"target": "Associativity",
+//"value": 1,
+//"Content":  "Vector Spaces are Associative."
+//        },
+
+//        {
+//"source": "Linear Combination",
+//"target": "Vector Space",
+//"value": 1,
+//"Content":  "Linear Combinations rely on the properties of a vector space. Specifically, Commutativity, Associativity, and Distributivity."
+//        },
+
+//        {
+//"source": "Vector Space",
+//"target": "Distributivity",
+//"value": 1,
+//"Content":  "Vector Spaces are Distributive."
+//        },
+
+//        {
+//"source": "Vector Space",
+//"target": "Additive Identity",
+//"value": 1,
+//"Content":  "Vector Spaces have additive identities."
+//        },
+
+//        {
+//"source": "Vector Space",
+//"target": "Multiplicative Identity",
+//"value": 1,
+//"Content":  "Vector Spaces have multiplicative identities."
+//        },
+
+//        {
+//"source": "Communication",
+//"target": "Explanation",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Subspaces",
+//"target": "Direct Sums",
+//"value": 1,
+//"Content":  "EMPTY"
+//        },
+
+//        {
+//"source": "Eigenvalues",
+//"target": "Eigenvectors",
+//"value": 1,
+//"Content":  "EMPTY"
+//        },
+
+//        {
+//"source": "Communication",
+//"target": "If/Only if Proofs",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Linear Independence",
+//"target": "Direct Proofs",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Groups",
+//"target": "Vector Space",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Communication",
+//"target": "Diagrams",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Diagrams",
+//"target": "Explanation",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Communication",
+//"target": "Proof by Contradiction",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "If/Only if Proofs",
+//"target": "Proof by Contradiction",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Direct Proofs",
+//"target": "If/Only if Proofs",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Eigenvalues",
+//"target": "Eigenvectors",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Vector Space",
+//"target": "Vectors",
+//"value": 1,
+//"Content":  "Vector Spaces contain Vectors."
+//        },
+
+//        {
+//"source": "Direct Sums",
+//"target": "If/Only if Proofs",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Direct Sums",
+//"target": "If/Only if Proofs",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Proof by Contrapositive",
+//"target": "Proof by Contradiction",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Multiplicative Identity",
+//"target": "Additive Identity",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Determinant",
+//"target": "Invertibility",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Invertibility",
+//"target": "Injectivity",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Invertibility",
+//"target": "Surjectivity",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Surjectivity",
+//"target": "Injectivity",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Vectors",
+//"target": "Linear Independence",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Vectors",
+//"target": "Linear Dependence",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Vectors",
+//"target": "Linear Combination",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Vectors",
+//"target": "Bases",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Vectors",
+//"target": "Span",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Vectors",
+//"target": "Groups",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Null Space",
+//"target": "Range",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Injectivity",
+//"target": "Null Space",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Surjectivity",
+//"target": "Range",
+//"value": 1,
+//"Content":  ""
+//        },
+
+//        {
+//"source": "Vectors",
+//"target": "Subspaces",
+//"value": 1,
+//"Content":  ""
+//        },
+//    ]
+//}
+
+
+
+
 let data = {
     "nodes": [
 	{
@@ -295,77 +810,70 @@ let data = {
 "source": "Communication",
 "target": "Explanation",
 "value": 1,
-"Content":  ""
+"Content":  "Explanation is a subset of Communication."
 	},
 
 	{
 "source": "Subspaces",
 "target": "Direct Sums",
 "value": 1,
-"Content":  "EMPTY"
-	},
-
-	{
-"source": "Eigenvalues",
-"target": "Eigenvectors",
-"value": 1,
-"Content":  "EMPTY"
+"Content":  "Direct Sums operate on Subspaces."
 	},
 
 	{
 "source": "Communication",
 "target": "If/Only if Proofs",
 "value": 1,
-"Content":  ""
+"Content":  "If/Only if Proofs are a form of Communication"
 	},
 
 	{
 "source": "Linear Independence",
 "target": "Direct Proofs",
 "value": 1,
-"Content":  ""
+"Content":  "Linear Independence can be proven through direct proofs."
 	},
 
 	{
 "source": "Groups",
 "target": "Vector Space",
 "value": 1,
-"Content":  ""
+"Content":  "Vector Spaces share many properties with Groups."
 	},
 
 	{
 "source": "Communication",
 "target": "Diagrams",
 "value": 1,
-"Content":  ""
+"Content":  "Diagrams are a method of communication."
 	},
 
 	{
 "source": "Diagrams",
 "target": "Explanation",
 "value": 1,
-"Content":  ""
+"Content":  "Diagrams are a method of explanation."
 	},
 
 	{
 "source": "Communication",
 "target": "Proof by Contradiction",
 "value": 1,
-"Content":  ""
+"Content":  "Proof by contradiction is a method of communicating a proof."
 	},
 
 	{
 "source": "If/Only if Proofs",
 "target": "Proof by Contradiction",
 "value": 1,
-"Content":  ""
+"Content":  "These are both types of proofs."
 	},
 
 	{
 "source": "Direct Proofs",
 "target": "If/Only if Proofs",
 "value": 1,
-"Content":  ""
+"Content":  "These are both types of proofs."
 	},
 
 	{
@@ -386,128 +894,245 @@ let data = {
 "source": "Direct Sums",
 "target": "If/Only if Proofs",
 "value": 1,
-"Content":  ""
-	},
-
-	{
-"source": "Direct Sums",
-"target": "If/Only if Proofs",
-"value": 1,
-"Content":  ""
+"Content":  "Proofs involving direct sums often are If/Only if Proofs."
 	},
 
 	{
 "source": "Proof by Contrapositive",
 "target": "Proof by Contradiction",
 "value": 1,
-"Content":  ""
+"Content":  "These are both types of proofs."
 	},
 
 	{
 "source": "Multiplicative Identity",
 "target": "Additive Identity",
 "value": 1,
-"Content":  ""
+"Content":  "These are both identity properties."
 	},
 
 	{
 "source": "Determinant",
 "target": "Invertibility",
 "value": 1,
-"Content":  ""
+"Content":  "A non-zero determinant mean a matrix is invertible."
 	},
 
 	{
 "source": "Invertibility",
 "target": "Injectivity",
 "value": 1,
-"Content":  ""
+"Content":  "Invertibility in linear maps requires injectivity."
 	},
 
 	{
 "source": "Invertibility",
 "target": "Surjectivity",
 "value": 1,
-"Content":  ""
+"Content":  "Invertibility in linear maps requires surjectivity."
 	},
 
 	{
 "source": "Surjectivity",
 "target": "Injectivity",
 "value": 1,
-"Content":  ""
+"Content":  "Injectivity and Surjectivity are both properties of Linear maps."
 	},
 
 	{
 "source": "Vectors",
 "target": "Linear Independence",
 "value": 1,
-"Content":  ""
+"Content":  "The property of linear independence is concerned with groups of vectors."
 	},
 
 	{
 "source": "Vectors",
 "target": "Linear Dependence",
 "value": 1,
-"Content":  ""
+"Content":  "The property of linear dependence is concerned with groups of vectors."
 	},
 
 	{
 "source": "Vectors",
 "target": "Linear Combination",
 "value": 1,
-"Content":  ""
+"Content":  "A linear combination is an operation involving addition and scalar multiplication of vectors."
 	},
 
 	{
 "source": "Vectors",
 "target": "Bases",
 "value": 1,
-"Content":  ""
+"Content":  "A basis is made up of vectors."
 	},
 
 	{
 "source": "Vectors",
 "target": "Span",
 "value": 1,
-"Content":  ""
+"Content":  "Spans include Vectors."
 	},
 
 	{
 "source": "Vectors",
 "target": "Groups",
 "value": 1,
-"Content":  ""
+"Content":  "Objects in groups can be vectors."
 	},
 
 	{
 "source": "Null Space",
 "target": "Range",
 "value": 1,
-"Content":  ""
+"Content":  "Null space and range both refer to the output of Linear Transformations."
 	},
 
 	{
 "source": "Injectivity",
 "target": "Null Space",
 "value": 1,
-"Content":  ""
+"Content":  "Injectivity means the null space is equal to zero."
 	},
 
 	{
 "source": "Surjectivity",
 "target": "Range",
 "value": 1,
-"Content":  ""
+"Content":  "A Linear Transformation is surjective if the range is equal to V."
 	},
 
 	{
 "source": "Vectors",
 "target": "Subspaces",
 "value": 1,
+"Content":  "Subspaces contain vectors."
+	},
+
+{
+"source": "Matrix Representation of Linear Maps",
+"target": "Linear Maps",
+"value": 1,
+"Content":  "Matrix Representations represent linear maps."
+	},
+
+{
+"source": "Eigenvalues",
+"target": "Linear Maps",
+"value": 1,
 "Content":  ""
+	},
+
+{
+"source": "Eigenvectors",
+"target": "Linear Maps",
+"value": 1,
+"Content":  ""
+	},
+
+{
+"source": "Vector Space",
+"target": "Additive Inverse",
+"value": 1,
+"Content":  "Vector spaces must contain additive inverses."
+	},
+
+{
+"source": "Vector Space",
+"target": "Invariant Subspaces",
+"value": 1,
+"Content":  ""
+	},
+
+{
+"source": "Vector Space",
+"target": "Linear Maps",
+"value": 1,
+"Content":  "Linear Maps operate on vector spaces."
+	},
+
+{
+"source": "Linear Maps",
+"target": "Injectivity",
+"value": 1,
+"Content":  "Injectivity is a property of linear maps."
+	},
+
+{
+"source": "Linear Maps",
+"target": "Surjectivity",
+"value": 1,
+"Content":  "Surjectivity is a property of linear maps."
+	},
+
+{
+"source": "Linear Maps",
+"target": "Invertibility",
+"value": 1,
+"Content":  "Invertibility is a property of linear maps."
+	},
+
+{
+"source": "Linear Maps",
+"target": "Null Space",
+"value": 1,
+"Content":  "The null space is the set of vectors that a linear map sends to zero."
+	},
+
+{
+"source": "Linear Maps",
+"target": "Range",
+"value": 1,
+"Content":  "The range is the set of all vectors a linear map outputs."
+	},
+
+{
+"source": "Diagrams",
+"target": "Determinant",
+"value": 1,
+"Content":  "Diagrams are useful for explaining determinants."
+	},
+
+{
+"source": "Diagrams",
+"target": "Subspaces",
+"value": 1,
+"Content":  "Diagrams are useful for explaining subspaces."
+	},
+
+{
+"source": "Diagrams",
+"target": "Null Space",
+"value": 1,
+"Content":  "Diagrams are useful for explaining the null space."
+	},
+
+{
+"source": "Diagrams",
+"target": "Range",
+"value": 1,
+"Content":  "Diagrams are useful for explaining the range."
+	},
+
+{
+"source": "Diagrams",
+"target": "Injectivity",
+"value": 1,
+"Content":  "Diagrams are useful for explaining injectivity."
+	},
+
+{
+"source": "Diagrams",
+"target": "Surjectivity",
+"value": 1,
+"Content":  "Diagrams are useful for explaining surjectivity."
 	},
     ]
 }
+
+
+
+
+
 
 export default data;
