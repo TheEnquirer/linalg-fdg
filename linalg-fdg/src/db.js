3194040e94ed207b2a718511131e0df76974dd6b
Exr0n <mail@exr0n.com>
2022-01-20 22:45:15 -0800

    secrets n stuff

--- linalg-fdg/src/db.js
+++ linalg-fdg/src/db.js
@@ -1,47 +1,55 @@
-import { useState, useEffect } from 'react';
+import { useReducer, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
 
 import data from './data';
+import { supabase_url, supabase_pubkey } from './secrets';
+
+const client = createClient(supabase_url, supabase_pubkey);
 
 /////////////////////////////////////////////////////
 // PUSH CHANGES TO DB
 /////////////////////////////////////////////////////
-//import { useState } from 'react';
 
-//import data from 'data';
+export async function addNode(obj) {
+    // TODO: impl
+}
 
-//const [ data_state, set_data ] = useState(data);
+export async function updateNode(delta) {
+    // TODO: impl
+}
 
-//export async function addNode(obj) {
-//    // TODO: impl
-//}
+export async function addEdge(obj) {
+    // TODO: impl
+}
 
-//export async function updateNode(delta) {
-//    // TODO: impl
-//}
-
-//export async function addEdge(obj) {
-//    // TODO: impl
-//}
-
-//export async function updateEdge(delta) {
-//    // TODO: impl
-//}
+export async function updateEdge(delta) {
+    // TODO: impl
+}
 
 /////////////////////////////////////////////////////
 // REACT TO DB CHANGES (haha get it? bc its react?? help)
 /////////////////////////////////////////////////////
 
 function useDatabase() {
-    const [ data_state, set_data ] = useState(data);
+    function reducer(state, action) {
+        console.log('reducer', state, action);
+    }
+    const [ data_state, dispatch ] = useReducer(reducer, data);
 
     useEffect(() => {
         // https://reactjs.org/docs/hooks-custom.html
-        return () => { /* unsubscribe */ }
+        //const subscriptions = client
+        //    .from('nodes').on('INSERT', handleNodeInsert)
+        //                  .on('UPDATE', handleNodeUpdate)
+        //    .from('edges').on('INSERT', handleEdgeInsert)
+        //                  .on('UPDATE', handleEdgeUpdate)
+        //    .subscribe();
+        const subscriptions = client.from('*').on('*', dispatch).subscribe();
+
+        return () => { client.removeSubscription(subscriptions); };
     });
 
     return data_state;
 }
 
 export default useDatabase;
-//export default data_state;
-
