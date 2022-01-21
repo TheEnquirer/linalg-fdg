84abf1a20d4438e4d865011e1776d0f4e03fef7b
Exr0n <mail@exr0n.com>
2022-01-20 22:55:29 -0800

    sharing secrets across projects doesn't work apparently

--- linalg-fdg/src/secrets.js
+++ linalg-fdg/src/secrets.js
@@ -1,3 +1,3 @@
-export const supabase_url = "https://mcuisxjoqzmwovztvaur.supabase.co";
-export const supabase_pubkey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTk0MjA2MywiZXhwIjoxOTU3NTE4MDYzfQ.C-tb83ZnUW-Fa_jtafT8tFg2jjY3PLvuujNehDzAYtw";
+export const supabase_url = "https://xowgdbirlorbplkgstmz.supabase.co";
+export const supabase_pubkey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQyNzQzNzcxLCJleHAiOjE5NTgzMTk3NzF9.auplOVy4eqa0s4FkemmdScRGXqSXkADqpcD6OBTKbPA";
 
